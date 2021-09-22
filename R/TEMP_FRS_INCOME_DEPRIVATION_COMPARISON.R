a1b2088b1b4d5d788d09964a3110c49b554be233
kygoffe <kayoung.goffe1@nhs.net>
2021-09-22 15:47:53 +0100

    package name change
    lowincomeschemeucd to nhslowincomeschemescrollytell

--- R/TEMP_FRS_INCOME_DEPRIVATION_COMPARISON.R
+++ R/TEMP_FRS_INCOME_DEPRIVATION_COMPARISON.R
@@ -1,7 +1,7 @@
 # # Map 1: 2019/2020 using FRS dataset as denominator
 # 
 # # Calculate take-up rate per region
-# region_df <- lowincomeschemeucd::target_population_df %>%
+# region_df <- nhslowincomeschemescrollytell::target_population_df %>%
 #   dplyr::filter(FINANCIAL_YEAR == "2019/20") %>%
 #   dplyr::filter(TYPE != "Child" &
 #                   !(AGE_BAND %in% c("0 to 15", "16 to 19 child")) &
@@ -10,16 +10,16 @@
 #   dplyr::group_by(FINANCIAL_YEAR, PCD_REGION_NAME) %>%
 #   dplyr::summarise(TOTAL_POPULATION = sum(TOTAL_POPULATION)) %>%
 #   dplyr::ungroup() %>%
-#   dplyr::inner_join(lowincomeschemeucd::successful_individuals_by_region_df) %>%
+#   dplyr::inner_join(nhslowincomeschemescrollytell::successful_individuals_by_region_df) %>%
 #   dplyr::mutate(value = TOTAL_SUCCESSFUL_INDIVIDUALS / TOTAL_POPULATION * 100,
 #                 drilldown = tolower(PCD_REGION_NAME)) %>%
 #   dplyr::select(FINANCIAL_YEAR, PCD_REGION_NAME, value, drilldown)
 # 
 # # Calculate rate per 1k adult population of each Local Authority
-# la_df <- lowincomeschemeucd::adult_population_df %>%
+# la_df <- nhslowincomeschemescrollytell::adult_population_df %>%
 #   dplyr::filter(FINANCIAL_YEAR == "2019/20") %>%
-#   dplyr::inner_join(lowincomeschemeucd::region_la_lookup) %>%
-#   dplyr::inner_join(lowincomeschemeucd::successful_individuals_by_la_df) %>%
+#   dplyr::inner_join(nhslowincomeschemescrollytell::region_la_lookup) %>%
+#   dplyr::inner_join(nhslowincomeschemescrollytell::successful_individuals_by_la_df) %>%
 #   dplyr::mutate(
 #     value = TOTAL_SUCCESSFUL_INDIVIDUALS / TOTAL_ADULT_POPULATION * 1000,
 #     drilldown = tolower(PCD_REGION_NAME)
@@ -31,14 +31,14 @@
 #                 drilldown)
 # 
 # # Join the region to the local authority map
-# la_map <- lowincomeschemeucd::la_map %>%
-#   dplyr::inner_join(lowincomeschemeucd::region_la_lookup)
+# la_map <- nhslowincomeschemescrollytell::la_map %>%
+#   dplyr::inner_join(nhslowincomeschemescrollytell::region_la_lookup)
 # 
 # # Create plot
 # highcharter::highchart(type = "map") %>%
 #   highcharter::hc_add_series(
 #     data = region_df,
-#     mapData = lowincomeschemeucd::region_map,
+#     mapData = nhslowincomeschemescrollytell::region_map,
 #     joinBy = "PCD_REGION_NAME",
 #     name = "regional take-up",
 #     tooltip = list(headerFormat = "",
