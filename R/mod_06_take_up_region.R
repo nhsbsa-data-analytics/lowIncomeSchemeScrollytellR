15269da8853703bb8b441b465e7d45ebaba16347
admivsn <adam.ivison@nhs.net>
2022-01-27 09:25:14 +0000

    Resize chart and move title to h6 instead of part of highcharter

--- R/mod_06_take_up_region.R
+++ R/mod_06_take_up_region.R
@@ -12,34 +12,37 @@ mod_06_take_up_region_ui <- function(id) {
   tagList(
     h4("Estimated take-up is low and decreasing over time"),
     p(
-      "Estimated take-up is the number of people ",
-      "who have received full or partial benefit ",
-      "per thousand of the general population aged 16 or over."
+      "Estimated take-up is the number of people who have received full or ",
+      "partial benefit per thousand of the general population aged 16 or over."
     ),
     br(),
     p(
       tags$b(
-        "Estimated take-up per thousand of the general population was 8 in 2015/16,",
-        "decreasing to 4 in 2020/21."
+        "Estimated take-up per thousand of the general population was 8 in ",
+        "2015/16, decreasing to 4 in 2020/21."
       )
     ),
     p(
       "By region, we can see that estimated take-up, relative to the ",
       "population, continues to be ",
       tags$b("highest in the North East of England"),
-      " and the North in general. Although the North East rate has ",
-      "declined from 13 in 2015/16 to 6 in 2020/21."
+      " and the North in general. Although the North East rate has declined ",
+      "from 13 in 2015/16 to 6 in 2020/21."
     ),
     fluidRow(
       align = "center",
       style = "background-color: #FFFFFF;",
+      h6(
+        "Estimated take-up of NHS Low Income Scheme in England (2015/16 to ",
+        "2020/21)"
+      ),
       highcharter::highchartOutput(
         outputId = ns("plot_successful_individuals_by_region"),
-        height = "600px"
+        height = "350px"
       )
     ),
     mod_download_ui(
-      id = ns("download_region_take_up")
+      id = ns("download_successful_individuals_by_region_df")
     )
   )
 }
@@ -51,13 +54,16 @@ mod_06_take_up_region_server <- function(id) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-
-    region_take_up <- lowIncomeSchemeScrollytellR::adult_population_df %>%
-      # dplyr::filter(FINANCIAL_YEAR == year()) %>%
+    # Calculate take-up rate per 1k adult population of each region
+    # Because of inner join from other data-raw output, calculating SDC here
+    successful_individuals_by_region_df <- 
+      lowIncomeSchemeScrollytellR::adult_population_df %>%
       dplyr::group_by(FINANCIAL_YEAR, PCD_REGION_NAME) %>%
       dplyr::summarise(TOTAL_POPULATION = sum(TOTAL_ADULT_POPULATION)) %>%
       dplyr::ungroup() %>%
-      dplyr::inner_join(lowIncomeSchemeScrollytellR::successful_individuals_by_region_df) %>%
+      dplyr::inner_join(
+        y = lowIncomeSchemeScrollytellR::successful_individuals_by_region_df
+      ) %>%
       dplyr::mutate(
         TAKE_UP_PER_THOUSAND = janitor::round_half_up(
           TOTAL_SUCCESSFUL_INDIVIDUALS / TOTAL_POPULATION * 1000, 1
@@ -66,14 +72,11 @@ mod_06_take_up_region_server <- function(id) {
       )
 
 
-
-    output$plot_successful_individuals_by_region <- highcharter::renderHighchart({
-
-      # Calculate take-up rate per 1k adult population of each region
-      # Because of inner join from other data-raw output, calculating SDC here
-
-      # Create plot
-      region_take_up %>%
+    # Create chart
+    output$plot_successful_individuals_by_region <- 
+      highcharter::renderHighchart({
+        
+        successful_individuals_by_region_df %>%
         highcharter::hchart(
           type = "line",
           highcharter::hcaes(
@@ -82,19 +85,8 @@ mod_06_take_up_region_server <- function(id) {
           )
         ) %>%
         theme_nhsbsa(stack = NA) %>%
-        highcharter::hc_title(
-          text = "Estimated take-up of NHS Low Income Scheme (2015/16 to 2020/21)"
-        ) %>%
-        # highcharter::hc_subtitle(
-        #   text = paste(
-        #     "Rates are rounded to the nearest whole number. <br> Hover",
-        #     " over the region in the legend to highlight."
-        #   ),
-        #   verticalAlign = "bottom",
-        #   align = "right"
-        # ) %>%
         highcharter::hc_yAxis(
-          title = list(text = "per thousand of the general population")
+          title = list(text = "Per thousand of the general population")
         ) %>%
         highcharter::hc_xAxis(
           title = list(text = "Financial year")
@@ -105,18 +97,17 @@ mod_06_take_up_region_server <- function(id) {
         )
     })
 
-    region_take_up_download <- region_take_up %>%
-      dplyr::rename(
-        REGION_NAME = PCD_REGION_NAME,
-        OVER16_POPULATION = TOTAL_POPULATION
-      )
-
-    # add data download
+    # Add data download
     mod_download_server(
-      id = "download_region_take_up",
-      filename = "region_take_up.csv",
-      export_data = region_take_up_download
+      id = "download_successful_individuals_by_region_df",
+      filename = "successful_individuals_by_region_df.csv",
+      export_data = successful_individuals_by_region_df %>%
+        dplyr::rename(
+          REGION_NAME = PCD_REGION_NAME,
+          OVER16_POPULATION = TOTAL_POPULATION
+        )
     )
+    
   })
 }
 
