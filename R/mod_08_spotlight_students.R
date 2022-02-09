9de3b0dc2b5ec6283639df877e52c285654d9342
admivsn <adam.ivison@nhs.net>
2022-02-09 09:14:59 +0000

    Reformat student map area and add table alongside

--- R/mod_08_spotlight_students.R
+++ R/mod_08_spotlight_students.R
@@ -30,21 +30,14 @@ mod_08_spotlight_students_ui <- function(id) {
       "applications. It has decreased further since the beginning of the ",
       "COVID-19 pandemic."
     ),
-    fluidRow(
-      align = "center",
-      style = "background-color: #FFFFFF;",
-      p(
-        tags$b(
-          "Number of NHS Low Income Scheme Student applications in England ",
-          "(2015/16 to 2020/21)"
-        )
-      ),
+    nhs_card(
+      heading = "Number of NHS Low Income Scheme Student applications in England (2015/16 to 2020/21)",
       highcharter::highchartOutput(
         outputId = ns("plot_student_applications"),
         height = "300px"
-      )
+      ),
+      mod_nhs_download_ui(id = ns("download_student_applications"))
     ),
-    mod_nhs_download_ui(id = ns("download_student_applications")),
     br(),
     p(
       tags$i(
@@ -62,15 +55,8 @@ mod_08_spotlight_students_ui <- function(id) {
       "resulting in full or partial benefit than non student applications ",
       "between 2015/16 and 2019/20."
     ),
-    fluidRow(
-      align = "center",
-      style = "background-color: #FFFFFF;",
-      p(
-        tags$b(
-          "Outcome of NHS Low Income Scheme Student applications in England ",
-          "(2015/16 to 2020/21)"
-        )
-      ),
+    nhs_card(
+      heading = "Outcome of NHS Low Income Scheme Student applications in England (2015/16 to 2020/21)",
       highcharter::highchartOutput(
         outputId = ns("plot_student_applications_by_outcome"),
         height = "300px"
@@ -79,9 +65,9 @@ mod_08_spotlight_students_ui <- function(id) {
         `for` = "play-range",
         class = "visuallyhidden",
         "year-range"
-      )
+      ),
+      mod_nhs_download_ui(id = ns("download_student_applications_by_outcome"))
     ),
-    mod_nhs_download_ui(id = ns("download_student_applications_by_outcome")),
     br(),
     p(
       tags$b("Estimated student take-up is also low and decreasing over time.")
@@ -100,45 +86,37 @@ mod_08_spotlight_students_ui <- function(id) {
       "Estimated take-up was around 4% in the 2015/16 Academic Year reducing ",
       "to 2% in 2019/20."
     ),
-    fluidRow(
-      col_5(
-        br(),
-        br(),
-        br(),
-        p(
-          "Estimated student take-up, continues to be somewhat",
-          tags$b(" lower in the South East and London."), "This appears to ",
-          "be primarily due to lower numbers of applications overall relative ",
-          "to the population."
-        ),
-        br(),
-        p("And student take-up is", tags$b("highest in the North West.")),
-        br(),
-        p(
-          "The take-up rate has",
-          tags$b("decreased most in the East of England"), "between 2015/16 ",
-          "and 2019/20."
-        )
-      ),
-      col_7(
-        style = "background-color: #FFFFFF;",
-        p(
-          tags$b(
-            "Estimated take-up of NHS Low Income Scheme Student individuals ",
-            "by England region 2015/16 to 2019/20"
+    p(
+      "Estimated student take-up, continues to be somewhat",
+      tags$b(" lower in the South East and London."), "This appears to be ",
+      "primarily due to lower numbers of applications overall relative to the ",
+      "population."
+    ),
+    p("And student take-up is", tags$b("highest in the North West.")),
+    p(
+      "The take-up rate has",
+      tags$b("decreased most in the East of England"), "between 2015/16 and ",
+      "2019/20."
+    ),
+    nhs_card(
+      heading = "Estimated take-up of NHS Low Income Scheme Student individuals by England region 2015/16 to 2019/20",
+      nhs_grid_2_col(
+        tags$div(
+          highcharter::highchartOutput(
+            outputId = ns("plot_successful_student_individuals_by_region"),
+            height = "500px"
+          ),
+          tags$label(
+            `for` = "play-range",
+            class = "visuallyhidden",
+            "year-range"
           )
         ),
-        highcharter::highchartOutput(
-          outputId = ns("plot_successful_student_individuals_by_region"),
-          height = "600px"
-        ),
-        tags$label(
-          `for` = "play-range",
-          class = "visuallyhidden",
-          "year-range"
-        ),
-        mod_nhs_download_ui(id = ns("download_student_individuals_by_region"))
-      )
+        DT::DTOutput(
+          outputId = ns("table_successful_student_individuals_by_region")
+        )
+      ),
+      mod_nhs_download_ui(id = ns("download_student_individuals_by_region"))
     )
   )
 }
@@ -274,7 +252,7 @@ mod_08_spotlight_students_server <- function(id) {
     )
 
     # Calculate rate per region
-    student_individuals_by_region <-
+    student_individuals_by_region_df <-
       lowIncomeSchemeScrollytellR::student_population_df %>%
       dplyr::filter(ACADEMIC_YEAR != "2014/15") %>%
       dplyr::inner_join(
@@ -289,12 +267,44 @@ mod_08_spotlight_students_server <- function(id) {
         TOTAL_SUCCESSFUL_STUDENT_INDIVIDUALS =
           round(TOTAL_SUCCESSFUL_STUDENT_INDIVIDUALS, -1)
       )
-
+    
+    # Create a table to go alongside the map
+    output$table_successful_student_individuals_by_region <- DT::renderDT(
+      expr = {
+        
+        # Format the table
+        student_individuals_by_region_df %>%
+          dplyr::arrange(desc(value)) %>%
+          dplyr::select(
+            "<span class='nhsuk-body-s'>Region</span>" := PCD_REGION_NAME,
+            "<span class='nhsuk-body-s'>Take up (%)</span>" := value
+          ) %>%
+          DT::datatable(
+            escape = FALSE,
+            options = list(
+              dom = "t",
+              scrollCollapse = TRUE,
+              paging = FALSE,
+              scrollY = "350px"
+            ),
+            height = "400px",
+            filter = "none"
+          ) %>%
+          DT::formatStyle(columns = 0:2, `font-size` = "14px") %>%
+          DT::formatRound(
+            columns = 2,
+            digits = 1
+          )
+      }
+    )
+    
+    
+    # Create chart
     output$plot_successful_student_individuals_by_region <-
       highcharter::renderHighchart({
 
         # Format for highcharter animation
-        plot_sequence_series <- student_individuals_by_region %>%
+        plot_sequence_series <- student_individuals_by_region_df %>%
           tidyr::complete(
             ACADEMIC_YEAR, PCD_REGION_NAME,
             fill = list(value = 0)
@@ -316,7 +326,7 @@ mod_08_spotlight_students_server <- function(id) {
             )
           ) %>%
           highcharter::hc_motion(
-            labels = unique(student_individuals_by_region$ACADEMIC_YEAR),
+            labels = unique(student_individuals_by_region_df$ACADEMIC_YEAR),
             startIndex = 4
           ) %>%
           theme_nhsbsa() %>%
@@ -327,9 +337,6 @@ mod_08_spotlight_students_server <- function(id) {
             ),
             align = "right"
           ) %>%
-          # highcharter::hc_subtitle(
-          #   text = "Students were allocated to a region based on the address on the application."
-          # ) %>%
           highcharter::hc_colorAxis(min = 0, max = 6) %>%
           highcharter::hc_legend(enabled = FALSE)
       })
@@ -340,6 +347,7 @@ mod_08_spotlight_students_server <- function(id) {
       export_data = student_individuals_by_region %>%
         dplyr::rename(STUDENT_TAKE_UP = value)
     )
+    
   })
 }
 
