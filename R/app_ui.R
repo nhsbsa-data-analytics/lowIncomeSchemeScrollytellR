c4c354cd013a29f6604ae3e747b6f1a888ae994a
kygoffe <kayoung.goffe1@nhs.net>
2022-02-14 16:54:44 +0000

    app_title change to user friendly way

--- R/app_ui.R
+++ R/app_ui.R
@@ -99,7 +99,7 @@ golem_add_external_resources <- function() {
     favicon(),
     bundle_resources(
       path = app_sys("app/www"),
-      app_title = "lowIncomeSchemeScrollytellR"
+      app_title = "Take-up of the NHS Low Income Scheme in England"
     )
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert()
